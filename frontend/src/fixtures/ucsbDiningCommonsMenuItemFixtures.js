3c25b480fc85242eaa40669efd263960bd26d4ff
Joshua Alexander <joshua.alexander0527@gmail.com>
2025-11-02 19:52:25 -0800

    fixing formatting for recommendation request tables

--- frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -1,28 +1,28 @@
-const ucsbDiningCommonsMenuItemFixtures= {
+const ucsbDiningCommonsMenuItemFixtures = {
   oneItem: {
-    id: 1, 
-    diningCommonsCode : "ortega",
-    name : "Cheeseburger",
-    station : "Grill",
+    id: 1,
+    diningCommonsCode: "ortega",
+    name: "Cheeseburger",
+    station: "Grill",
   },
   threeItems: [
     {
       id: 1,
-      diningCommonsCode : "ortega",
-      name : "Cheeseburger",
-      station : "Grill",
+      diningCommonsCode: "ortega",
+      name: "Cheeseburger",
+      station: "Grill",
     },
     {
       id: 2,
-      diningCommonsCode : "portola",
-      name : "Veggie Burger",
-      station : "Counter",
+      diningCommonsCode: "portola",
+      name: "Veggie Burger",
+      station: "Counter",
     },
     {
       id: 3,
-      diningCommonsCode : "de-la-guerra",
-      name : "Chicken Sandwich",
-      station : "specials"
+      diningCommonsCode: "de-la-guerra",
+      name: "Chicken Sandwich",
+      station: "specials",
     },
   ],
 };
